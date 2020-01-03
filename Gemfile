e80787188b24a14ed5b60abfd05efcde1b3167e5
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-01-03 13:36:40 -0300

    Add and setup simplecov gem

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gemspec
 
 gem "rake", "~> 12.0"
 gem "minitest", "~> 5.0"
+gem "simplecov", "~> 0.17.1"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/test/"
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "kind"
 
