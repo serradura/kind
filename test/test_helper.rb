7bc93ca109e5f440438bdca0c0bd235e8810a59c
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-04-09 00:29:52 -0300

    Add custom assertions to verify exceptions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,17 @@ require "kind"
 
 require "minitest/pride"
 require "minitest/autorun"
+
+class Minitest::Test
+  def assert_raises_with_message(exception, msg, &block)
+    block.call
+  rescue exception => e
+    assert_match(msg, e.message)
+  else
+    raise "Expected to raise #{exception} w/ message #{msg}, none raised"
+  end
+
+  def assert_raises_kind_error(message, &block)
+    assert_raises_with_message(Kind::Error, message, &block)
+  end
+end
