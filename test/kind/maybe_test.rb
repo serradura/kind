a154fc11cdf172aba34aea56f9a94fd5eb059c8c
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-04-12 09:26:46 -0300

    Transform Kind::Optional into Kind::Maybe

--- test/kind/maybe_test.rb
+++ test/kind/maybe_test.rb
@@ -1,32 +1,32 @@
 require 'test_helper'
 
-class Kind::OptionalTest < Minitest::Test
-  def test_optional_constructor
-    optional = Kind::Optional.new(0)
+class Kind::MaybeTest < Minitest::Test
+  def test_maybe_constructor
+    optional = Kind::Maybe.new(0)
 
-    assert_equal(0, Kind::Optional.new(optional).value)
+    assert_equal(0, Kind::Maybe.new(optional).value)
   end
 
-  def test_optional_value
-    optional1 = Kind::Optional.new(2)
+  def test_maybe_value
+    optional1 = Kind::Maybe.new(2)
 
     assert_equal(2, optional1.value)
 
     # ---
 
-    optional2 = Kind::Optional.new(nil)
+    optional2 = Kind::Maybe.new(nil)
 
     assert_nil(optional2.value)
 
     # ---
 
-    optional3 = Kind::Optional.new(Kind::Undefined)
+    optional3 = Kind::Maybe.new(Kind::Undefined)
 
     assert_equal(Kind::Undefined, optional3.value)
   end
 
-  def test_value_or_default
-    optional1 = Kind::Optional.new(2)
+  def test_maybe_value_or_default
+    optional1 = Kind::Maybe.new(2)
 
     assert_equal(2, optional1.value_or(0))
 
@@ -34,7 +34,7 @@ class Kind::OptionalTest < Minitest::Test
 
     # ---
 
-    optional2 = Kind::Optional.new(nil)
+    optional2 = Kind::Maybe.new(nil)
 
     assert_equal(0, optional2.value_or(0))
 
@@ -47,7 +47,7 @@ class Kind::OptionalTest < Minitest::Test
 
     # ---
 
-    optional3 = Kind::Optional.new(Kind::Undefined)
+    optional3 = Kind::Maybe.new(Kind::Undefined)
 
     assert_equal(1, optional3.value_or(1))
 
@@ -59,22 +59,22 @@ class Kind::OptionalTest < Minitest::Test
     ) { optional3.value_or }
   end
 
-  def test_optional_when_some
-    assert_predicate(Kind::Optional.new(2), :some?)
+  def test_maybe_when_some
+    assert_predicate(Kind::Maybe.new(2), :some?)
 
-    refute_predicate(Kind::Optional.new(nil), :some?)
-    refute_predicate(Kind::Optional.new(Kind::Undefined), :some?)
+    refute_predicate(Kind::Maybe.new(nil), :some?)
+    refute_predicate(Kind::Maybe.new(Kind::Undefined), :some?)
   end
 
-  def test_optional_when_none
-    assert_predicate(Kind::Optional.new(nil), :none?)
-    assert_predicate(Kind::Optional.new(Kind::Undefined), :none?)
+  def test_maybe_when_none
+    assert_predicate(Kind::Maybe.new(nil), :none?)
+    assert_predicate(Kind::Maybe.new(Kind::Undefined), :none?)
 
-    refute_predicate(Kind::Optional.new(1), :none?)
+    refute_predicate(Kind::Maybe.new(1), :none?)
   end
 
-  def test_optional_map_when_none
-    optional1 = Kind::Optional.new(2)
+  def test_maybe_map_when_none
+    optional1 = Kind::Maybe.new(2)
     optional2 = optional1.map(&:to_s)
     optional3 = optional2.map { |value| value * 2 }
 
@@ -90,8 +90,8 @@ class Kind::OptionalTest < Minitest::Test
     refute_same(optional2, optional3)
   end
 
-  def test_optional_when_map_returns_nil
-    optional1 = Kind::Optional.new(2)
+  def test_maybe_when_map_returns_nil
+    optional1 = Kind::Maybe.new(2)
     optional2 = optional1.map { nil }
     optional3 = optional2.map { |value| value * 2 }
 
@@ -106,8 +106,8 @@ class Kind::OptionalTest < Minitest::Test
     assert_predicate(optional3, :none?)
   end
 
-  def test_optional_when_map_returns_undefined
-    optional1 = Kind::Optional.new(3)
+  def test_maybe_when_map_returns_undefined
+    optional1 = Kind::Maybe.new(3)
     optional2 = optional1.map { Kind::Undefined }
     optional3 = optional2.map { |value| value * 3 }
 
@@ -122,13 +122,16 @@ class Kind::OptionalTest < Minitest::Test
     assert_predicate(optional3, :none?)
   end
 
-  def test_optional_constructor_alias
-    assert_instance_of(Kind::Optional, Kind::Optional[1])
+  def test_maybe_constructor_alias
+    assert_instance_of(Kind::Maybe::Some, Kind::Maybe[1])
+
+    assert_instance_of(Kind::Maybe::None, Kind::Maybe[nil])
+    assert_instance_of(Kind::Maybe::None, Kind::Maybe[Kind::Undefined])
   end
 
   def test_then_as_an_alias_of_map
     result1 =
-      Kind::Optional[5]
+      Kind::Maybe[5]
         .then { |value| value * 5 }
         .then { |value| value + 17 }
         .value_or(0)
@@ -138,7 +141,7 @@ class Kind::OptionalTest < Minitest::Test
     # ---
 
     result2 =
-      Kind::Optional[5]
+      Kind::Maybe[5]
         .then { nil }
         .value_or { 1 }
 
@@ -147,7 +150,7 @@ class Kind::OptionalTest < Minitest::Test
     # ---
 
     result3 =
-      Kind::Optional[5]
+      Kind::Maybe[5]
         .then { Kind::Undefined }
         .value_or(-2)
 
@@ -155,17 +158,42 @@ class Kind::OptionalTest < Minitest::Test
   end
 
   def test_try_method
-    assert_equal('FOO', Kind::Optional['foo'].try(:upcase))
-    assert_equal('FOO', Kind::Optional['foo'].try { |value| value.upcase })
+    assert_equal('FOO', Kind::Maybe['foo'].try(:upcase))
+    assert_equal('FOO', Kind::Maybe['foo'].try { |value| value.upcase })
 
-    assert_nil(Kind::Optional[nil].try(:upcase))
-    assert_nil(Kind::Optional[nil].try { |value| value.upcase })
+    assert_nil(Kind::Maybe[nil].try(:upcase))
+    assert_nil(Kind::Maybe[nil].try { |value| value.upcase })
 
-    assert_nil(Kind::Optional[Kind::Undefined].try(:upcase))
-    assert_nil(Kind::Optional[Kind::Undefined].try { |value| value.upcase })
+    assert_nil(Kind::Maybe[Kind::Undefined].try(:upcase))
+    assert_nil(Kind::Maybe[Kind::Undefined].try { |value| value.upcase })
 
     assert_raises_with_message(Kind::Error, '"upcase" expected to be a kind of Symbol') do
-      Kind::Optional['foo'].try('upcase')
+      Kind::Maybe['foo'].try('upcase')
     end
+  end
+
+  def test_optional_is_maybe
+    assert_equal(Kind::Maybe, Kind::Optional)
+
+    # ---
+
+    result1 =
+      Kind::Optional
+        .new(5)
+        .map { |value| value * 5 }
+        .map { |value| value - 10 }
+        .value_or(0)
+
+    assert_equal(15, result1)
+
+    # ---
+
+    result2 =
+      Kind::Optional[5]
+        .then { |value| value * 5 }
+        .then { |value| value + 10 }
+        .value_or(0)
+
+    assert_equal(35, result2)
   end
 end
