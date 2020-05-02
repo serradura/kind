fb44c5a7e976c3f7884b0ae9924152aa913ad485
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-05-02 12:07:07 -0300

    Fix Kind.is module verifications

--- test/kind/is_module_test.rb
+++ test/kind/is_module_test.rb
@@ -8,13 +8,13 @@ class Kind::IsClassTest < Minitest::Test
   end
 
   def test_if_a_value_is_an_enumerable_module
-    assert_kind_is(:Enumerable, Enumerable)
+    assert_kind_is(:Enumerable, Enumerable, Module.new { extend(Enumerable) })
 
     refute_kind_is(:Enumerable, Comparable, Module)
   end
 
   def test_if_a_value_is_a_comparable_module
-    assert_kind_is(:Comparable, Comparable)
+    assert_kind_is(:Comparable, Comparable, Module.new { extend(Comparable) })
 
     refute_kind_is(:Comparable, Enumerable, Module)
   end
