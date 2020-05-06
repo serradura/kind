6b14d8a5c1a0dd4ff7537c6f72bfe18f0233b7f8
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-05-06 00:43:43 -0300

    Add kind/active_model/validation.rb

---
+++ lib/kind/validator.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Kind
+  module Validator
+    DEFAULT_STRATEGIES = Set.new(%w[instance_of kind_of is_a]).freeze
+
+    class InvalidDefinition < ArgumentError
+      OPTIONS = 'Options to define one: :of, :instance_of, :respond_to, :klass, :array_of or :array_with'.freeze
+
+      def initialize(attribute)
+        super "invalid type definition for :#{attribute} attribute. #{OPTIONS}"
+      end
+
+      private_constant :OPTIONS
+    end
+
+    class InvalidDefaultStrategy < ArgumentError
+      OPTIONS =
+        DEFAULT_STRATEGIES.map { |option| ":#{option}" }.join(', ')
+
+      def initialize(option)
+        super "#{option.inspect} is an invalid option. Please use one of these: #{OPTIONS}"
+      end
+
+      private_constant :OPTIONS
+    end
+
+    def self.default_strategy
+      @default_strategy ||= :kind_of
+    end
+
+    def self.default_strategy=(option)
+      if DEFAULT_STRATEGIES.member?(String(option))
+        @default_strategy = option.to_sym
+      else
+        raise InvalidDefaultStrategy.new(option)
+      end
+    end
+  end
+end
---
+++ test/kind/active_model/validation/respond_to_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+if ENV['ACTIVEMODEL_VERSION']
+  class KindActiveModelValidationRespondToTest < Minitest::Test
+    class Person
+      include ActiveModel::Validations
+
+      attr_reader :handler
+
+      validates :handler, kind: { respond_to: :call }, allow_nil: true
+
+      def initialize(handler:)
+        @handler = handler
+      end
+    end
+
+    def test_the_respond_to_validation
+      invalid_person = Person.new(handler: 21)
+
+      refute_predicate(invalid_person, :valid?)
+      assert_equal(['must respond to the method `call`'], invalid_person.errors[:handler])
+
+      person = Person.new(handler: -> {})
+
+      assert_predicate(person, :valid?)
+    end
+
+    def test_the_allow_nil_validation_options
+      person = Person.new(handler: nil)
+
+      assert_predicate(person, :valid?)
+    end
+
+    # ---
+
+    class Task
+      include ActiveModel::Validations
+
+      attr_reader :handler
+
+      validates! :handler, kind: { respond_to: :call }, allow_nil: true
+
+      def initialize(handler:)
+        @handler = handler
+      end
+    end
+
+    def test_strict_validations
+      assert_predicate(Task.new(handler: nil), :valid?)
+      assert_predicate(Task.new(handler: -> {}), :valid?)
+
+      assert_raises_kind_error('handler must respond to the method `call`') do
+        Task.new(handler: 42).valid?
+      end
+    end
+  end
+end
