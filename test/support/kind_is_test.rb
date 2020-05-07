8a106f87d2df17f6cfc2f03422e7526f581f9d01
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-05-06 23:45:09 -0300

    Improve test/kind/is_test.rb

--- test/support/kind_is_test.rb
+++ test/support/kind_is_test.rb
@@ -15,32 +15,34 @@ module KindIsTest
   module Person3; extend Human3; end
   module User3; extend Human3; end
 
-  class Base
-    include ActiveModel::Validations
-    attr_accessor :human_kind
-  end
+  if ENV['ACTIVEMODEL_VERSION']
+    class Base
+      include ActiveModel::Validations
+      attr_accessor :human_kind
+    end
 
-  class Class1 < Base
-    validates :human_kind, kind: { is: Human1 }, allow_nil: true
-  end
+    class Class1 < Base
+      validates :human_kind, kind: { is: Human1 }, allow_nil: true
+    end
 
-  class StrictClass1 < Base
-    validates! :human_kind, kind: { is: Human1 }, allow_nil: true
-  end
+    class StrictClass1 < Base
+      validates! :human_kind, kind: { is: Human1 }, allow_nil: true
+    end
 
-  class Class2 < Base
-    validates :human_kind, kind: { is: Human2 }, allow_nil: true
-  end
+    class Class2 < Base
+      validates :human_kind, kind: { is: Human2 }, allow_nil: true
+    end
 
-  class StrictClass2 < Base
-    validates! :human_kind, kind: { is: Human2 }, allow_nil: true
-  end
+    class StrictClass2 < Base
+      validates! :human_kind, kind: { is: Human2 }, allow_nil: true
+    end
 
-  class Class3 < Base
-    validates :human_kind, kind: { is: Human3 }, allow_nil: true
-  end
+    class Class3 < Base
+      validates :human_kind, kind: { is: Human3 }, allow_nil: true
+    end
 
-  class StrictClass3 < Base
-    validates! :human_kind, kind: { is: Human3 }, allow_nil: true
+    class StrictClass3 < Base
+      validates! :human_kind, kind: { is: Human3 }, allow_nil: true
+    end
   end
 end
