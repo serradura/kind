9ccd7c599ebfd82b85b8962e28f016378e5b8e74
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2021-02-28 17:20:50 -0300

    Add Kind::RespondTo (new core component)

---
+++ lib/kind/core/respond_to.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Kind
+  class RespondTo
+    def self.[](*args)
+      args.each { |arg| KIND.of!(::Symbol, arg) }
+
+      new(args)
+    end
+
+    private_class_method :new
+
+    def initialize(method_names)
+      @method_names = method_names
+    end
+
+    def |(another_kind)
+      UnionType.new(self) | another_kind
+    end
+
+    def ===(value)
+      KIND.interface?(@method_names, value)
+    end
+
+    alias_method :call, :===
+
+    def [](value)
+      KIND.of!(self, value)
+    end
+
+    def inspect
+      @inspect ||= "Kind::RespondTo#{@method_names}"
+    end
+
+    alias_method :name, :inspect
+  end
+end
---
+++ test/kind/core/respond_to_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class Kind::RespondToTest < Minitest::Test
+  def test_the_constructor
+    assert_raises_with_message(
+      NoMethodError,
+      "private method `new' called for Kind::RespondTo:Class"
+    ) { Kind::RespondTo.new([:fetch]) }
+
+    assert_raises_with_message(
+      Kind::Error,
+      '1 expected to be a kind of Symbol'
+    ) { Kind::RespondTo[1] }
+  end
+
+  RespondToFetch = Kind::RespondTo[:fetch]
+  RespondToFetchAndValuesAt = Kind::RespondTo[:fetch, :values_at]
+
+  def test_the_inspect_method
+    assert_equal('Kind::RespondTo[:fetch]', RespondToFetch.inspect)
+    assert_equal('Kind::RespondTo[:fetch, :values_at]', RespondToFetchAndValuesAt.inspect)
+
+    assert_equal(RespondToFetch.name, RespondToFetch.inspect)
+    assert_equal(RespondToFetchAndValuesAt.name, RespondToFetchAndValuesAt.inspect)
+  end
+
+  def test_the_method_verification
+    assert RespondToFetch === {}
+    refute RespondToFetch === ''
+
+    assert RespondToFetchAndValuesAt === {}
+    refute RespondToFetchAndValuesAt === 1
+
+    assert RespondToFetch.({})
+    refute RespondToFetch.(1)
+
+    assert RespondToFetchAndValuesAt.({})
+    refute RespondToFetchAndValuesAt.('')
+
+    assert_equal({}, RespondToFetch[{}])
+    assert_equal({}, RespondToFetchAndValuesAt[{}])
+
+    assert_raises_with_message(
+      Kind::Error,
+      '1 expected to be a kind of Kind::RespondTo[:fetch]'
+    ) { RespondToFetch[1] }
+
+    assert_raises_with_message(
+      Kind::Error,
+      '1 expected to be a kind of Kind::RespondTo[:fetch, :values_at]'
+    ) { RespondToFetchAndValuesAt[1] }
+  end
+
+  def test_the_union_type_builder
+    interface = Kind::RespondTo[:fetch] | Kind::Nil
+
+    assert interface === nil
+    assert interface === []
+    assert interface === {}
+
+    refute interface === 1
+  end
+end
